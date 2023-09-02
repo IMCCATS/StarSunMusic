edb57fde9c371bee6b635cc639a6cae758607db5
IMCCATS <cgyxjs@qq.com>
2023-09-02 22:33:52 +0800

    免责声明全局添加

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,6 @@ import DashboardIcon from "@mui/icons-material/Dashboard";
 import CircularProgress from "@mui/material/CircularProgress";
 import { useRouter } from "next/navigation";
 
-
 const HomePage = () => {
   const router = useRouter();
   const [isLoading, setIsLoading] = React.useState(false);
@@ -63,6 +62,10 @@ const HomePage = () => {
             textAlign: "center",
           }}
         >
+          <span>
+            免责声明：本网站内容来源于网络，仅提供检索服务。本网站不存储任何内容，也不承担任何责任。如果您需要使用本网站提供的检索服务，您应当自行负责获取相关内容的合法性和准确性。此外，本网站保留随时修改本声明的权利。如果您继续使用本网站，即表示您已经接受了本声明的所有修改。
+          </span>
+          <br />
           <span>开发者：小研同学</span>
           <br />
           <span>运营：内蒙古畅哥计算机科技工作室</span>
@@ -70,11 +73,9 @@ const HomePage = () => {
           <span>本系统仅用作个人音乐欣赏、学习交流，不可用于商业用途。</span>
           <br />
           <span>
-            &copy; 2020- {new Date().getFullYear()} 内蒙古畅哥计算机科技工作室
+            &copy; 2020 - {new Date().getFullYear()} 内蒙古畅哥计算机科技工作室
             版权所有.
           </span>
-          <br />
-          <span>音乐资源来自于网络，其版权归属音乐原版权方所有。</span>
           <br />
           <span>应用程序可能响应较慢或无响应，烦请耐心等待，多次尝试~</span>
           <br />
