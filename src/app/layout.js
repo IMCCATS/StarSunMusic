6d09c245a7b9c2521279326a2b4f3276eb8e192d
IMCCATS <cgyxjs@qq.com>
2023-09-25 00:58:20 +0800

    404页面定义，修复加载慢的问题

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import "./main.css";
 
 export const metadata = {
-  title: "起始页 · 星阳音乐系统",
-  description: "起始页 · 星阳音乐系统",
+  title: "星阳音乐系统",
+  description: "星阳音乐系统",
 };
 
 export default function RootLayout(props) {
