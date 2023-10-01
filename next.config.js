086be997f035328261045645d25f98a017c91c9b
IMCCATS <cgyxjs@qq.com>
2023-10-01 17:33:41 +0800

    恢复

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  async headers() {
+    return [
+      {
+        source: "/src/favicon.ico",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "image/x-icon",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
