ffe695c549830ad866aab5df83bd4f7f8d659909
IMCCATS <cgyxjs@qq.com>
2023-10-14 23:40:42 +0800

    去除捐赠入口

--- components/common/juanzeng.js
+++ components/common/juanzeng.js
@@ -1,17 +1,8 @@
 "use client";
-import React, { useEffect, useRef } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 export default function JuanZeng() {
-  const iframeRef = useRef(null);
-  useEffect(() => {
-    if (document.body.clientWidth < 700) {
-      iframeRef.current.style.width = "100%";
-    } else {
-      iframeRef.current.style.width = "640px";
-    }
-  }, []);
   return (
     <Card sx={{ minWidth: 275 }} style={{ marginTop: "15px" }}>
       <CardContent>
@@ -35,35 +26,6 @@ export default function JuanZeng() {
             而且，在这个世界上，有那么多需要帮助的人，而公益事业，就是那道温暖的光，照亮他们的未来。对公益事业的支持，就是对我们的支持。参与公益事业，一起为这个世界贡献一份力量。让我们一起用互联网的力量，推动公益事业的发展。因为您的参与，世界将变得更加美好。
           </span>
         </Typography>
-        <Typography variant="body2">
-          <span>
-            如果您愿意支持我们的开发事业，想要使本系统更加完美，您可以到爱发电平台为我们的开发事业“发电”。我们将定期抽出一部分发电所得资金投入公益事业，为这个世界贡献一份力量。
-            <br />
-            点击下方的卡片，您将被引导至爱发电平台，该平台为我们提供技术支持。
-            <br />
-            爱发电平台信息：琼公网安备 46010602000751号 | 琼ICP备18001024号-1 |
-            琼网文: [2020]3177-105号
-          </span>
-        </Typography>
-        <Card>
-          <CardContent>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                justifyItems: "center",
-              }}
-            >
-              <iframe
-                ref={iframeRef}
-                src="https://afdian.net/leaflet?slug=SingTStudios"
-                scrolling="no"
-                style={{ width: "100%", height: "200px" }}
-                frameBorder="0"
-              />
-            </div>
-          </CardContent>
-        </Card>
       </CardContent>
     </Card>
   );
