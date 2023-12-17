4a61651e6e477c47210ece9398cc3db6fd25b441
IMCCATS <cgyxjs@qq.com>
2023-12-17 12:10:24 +0800

    修改

--- src/app/api/appconfig.js
+++ src/app/api/appconfig.js
@@ -1,3 +1,3 @@
-export const appversion = "1.0.0_202312012314"; //当前版本
-export const appupdatetime = "2023-12-01"; //更新时间
+export const appversion = "1.0.0_2023121765145"; //当前版本
+export const appupdatetime = "2023-12-17"; //更新时间
 export const appupdatecontent = "修复了一些已知问题；"; //更新内容
