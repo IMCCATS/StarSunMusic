055c5efb7a8f2420449ee770867df75e07d7b450
IMCCATS <cgyxjs@qq.com>
2024-02-09 20:30:14 +0800

    百度统计

--- components/Settings/Settings.js
+++ components/Settings/Settings.js
@@ -1,27 +1,63 @@
 import yuxStorage from "@/app/api/yux-storage";
-import { Card, Flex, Typography, Switch, message, List } from "antd";
+import { Card, Typography, Switch, message, List } from "antd";
 import * as React from "react";
 const { Text } = Typography;
 export default function SystemSettings() {
 	//变量定义
 	const [messageApi, contextHolder] = message.useMessage();
-	const [CannotPlaySwitchState, setCannotPlaySwitchState] =
+	const [PlaylistEndSwitchState, setPlaylistEndSwitchState] =
+		React.useState(null);
+	const [ListPlayFailedSwitchState, setListPlayFailedSwitchState] =
 		React.useState(null);
 	//函数
+
+	const data = [
+		{
+			label: "列表播放完成时操作",
+			description: "当列表播放时列表歌曲全部播放完成时的操作",
+			enableLabel: "语音提示",
+			disabledLabel: "关闭提醒",
+			switchState: PlaylistEndSwitchState,
+			switchOnchange: PlaylistEndSwitchOnChange,
+		},
+		{
+			label: "列表播放失败时操作",
+			description: "当列表播放时有歌曲播放失败时的操作",
+			enableLabel: "语音提示",
+			disabledLabel: "关闭提醒",
+			switchState: ListPlayFailedSwitchState,
+			switchOnchange: ListPlayFailedSwitchOnChange,
+		},
+	];
+
 	React.useEffect(() => {
 		yuxStorage
-			.getItem("handleCannotPlay")
+			.getItem("handlePlaylistEnd")
 			.then((e) => {
 				if (e === "0") {
-					setCannotPlaySwitchState(false);
+					setPlaylistEndSwitchState(false);
 				} else {
-					setCannotPlaySwitchState(true);
+					setPlaylistEndSwitchState(true);
 				}
 			})
 			.catch((e) => {
-				setCannotPlaySwitchState(true);
+				setPlaylistEndSwitchState(true);
 			});
+		yuxStorage
+			.getItem("handleListPlayFailed")
+			.then((e) => {
+				if (e === "0") {
+					setListPlayFailedSwitchState(false);
+				} else {
+					setListPlayFailedSwitchState(true);
+				}
+			})
+			.catch((e) => {
+				setListPlayFailedSwitchState(true);
+			});
+		setac(data);
 	}, []);
+
 	/**
 	 * @description 本地保存设置项
 	 * @author 韩研凌(小研同学)
@@ -42,17 +78,17 @@ export default function SystemSettings() {
 		});
 	};
 	/**
-	 * @description 操作保存：无法播放时操作
+	 * @description 操作保存：播放列表完成时事件
 	 * @author 韩研凌(小研同学)
 	 * @date 2024/02/08
 	 * @param {boolean} state 设置状态：语音提示为1（默认），直接跳过为0
 	 */
-	const CannotPlaySwitchOnChange = (state) => {
+	const PlaylistEndSwitchOnChange = (state) => {
 		let a = "0";
 		if (state) {
 			a = "1";
 		}
-		SaveSetting("handleCannotPlay", a)
+		SaveSetting("handlePlaylistEnd", a)
 			.then((e) => {
 				messageApi.success("设置成功");
 			})
@@ -61,20 +97,26 @@ export default function SystemSettings() {
 			});
 	};
 
-	const data = [
-		{
-			label: "列表播放完成时操作",
-			description: "当列表播放时列表歌曲全部播放完成时的操作",
-			enableLabel: "语音提示",
-			disabledLabel: "关闭提醒",
-			switchState: CannotPlaySwitchState,
-			switchOnchange: CannotPlaySwitchOnChange,
-		},
-	];
+	const ListPlayFailedSwitchOnChange = (state) => {
+		let a = "0";
+		if (state) {
+			a = "1";
+		}
+		SaveSetting("handleListPlayFailed", a)
+			.then((e) => {
+				messageApi.success("设置成功");
+			})
+			.catch((e) => {
+				messageApi.error("设置失败");
+			});
+	};
+
+	const [ac, setac] = React.useState([]);
+
 	return (
 		<>
 			{contextHolder}
-			{isNaN(CannotPlaySwitchState) ? (
+			{isNaN(PlaylistEndSwitchState) || isNaN(ListPlayFailedSwitchState) ? (
 				<Text>设置加载中...</Text>
 			) : (
 				<Card>
@@ -85,7 +127,7 @@ export default function SystemSettings() {
 					<List
 						style={{ marginTop: "10px" }}
 						bordered
-						dataSource={data}
+						dataSource={ac}
 						renderItem={(item) => (
 							<List.Item
 								extra={
@@ -104,15 +146,6 @@ export default function SystemSettings() {
 							</List.Item>
 						)}
 					/>
-					{/* <>
-								<Text strong>无法播放时操作</Text>
-								<Switch
-									checkedChildren="语音提示"
-									unCheckedChildren="直接跳过"
-									value={CannotPlaySwitchState}
-									onChange={CannotPlaySwitchOnChange}
-								/>
-							</> */}
 				</Card>
 			)}
 		</>
