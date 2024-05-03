1b92e141a531ffa95c7b182fc825a40472c58952
IMCCATS <cgyxjs@qq.com>
2024-05-03 17:12:22 +0800

    Seo

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,6 +1,7 @@
 export const metadata = {
-	title: "首页 · 星阳音乐系统",
-	description: "首页 · 星阳音乐系统",
+	title: "星阳音乐系统  - 让音韵流传 让乐章永恒",
+	description:
+		"创建令人沉醉的音乐之旅，从我们的创新音乐系统开始。探索无尽的旋律空间，享受高清音质带来的每一次心灵触碰。",
 };
 
 export default function RootLayout(props) {
